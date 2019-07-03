961aedeacef7dc632242963a05b5d1996278b05a
Adhish Ramkumar <adhishramkumar@yahoo.com>
2019-07-03 14:12:56 -0400

    game configuration

--- avalon-app/src/view/game/gameConfiguration.tsx
+++ avalon-app/src/view/game/gameConfiguration.tsx
@@ -7,6 +7,7 @@ import {
     ControlGroup,
     HTMLSelect,
     Intent,
+    Callout,
 } from "@blueprintjs/core";
 import styles from "./gameConfiguration.module.scss";
 import { ContextType, getServices } from "../../common/contextProvider";
@@ -15,6 +16,7 @@ import { Player } from "./player";
 import { handleStringChange } from "../../common/handleStringChange";
 import { IconNames } from "@blueprintjs/icons";
 import { removeElementAtIndex } from "../../common/remove";
+import { assertNever } from "../../common/assertNever";
 
 interface IGameConfigurationProps {
     game: IGame;
@@ -57,6 +59,10 @@ export class GameConfiguration extends React.PureComponent<IGameConfigurationPro
         PLAYER_ORDER: "Player order",
         ROLES: "Roles",
         ADD_ROLE: "Add role",
+        START_GAME: "Start game",
+        NOT_ENOUGH_ROLES: "There are more players than roles specified.",
+        TOO_MANY_ROLES: "There too many roles specified.",
+        TAB_NAVIGATION: "TabNavigation",
     }
     private services = getServices(this.context);
 
@@ -70,22 +76,36 @@ export class GameConfiguration extends React.PureComponent<IGameConfigurationPro
         return (
             <div>
                 <H2 className={styles.configurationHeader}>{STRINGS.CONFIGURE_GAME}</H2>
-                <Tabs id="TabsExample" onChange={this.handleTabChange} selectedTabId="rx">
-                    <Tab
-                        id={ConfigurationTab.PLAYERS}
-                        title={STRINGS.PLAYER_ORDER}
-                        panel={<div>{this.renderPlayers()}</div>} />
-                    <Tab
-                        id={ConfigurationTab.ROLES}
-                        title={STRINGS.ROLES}
-                        panel={this.renderRoleSelect()} />
+                <Tabs
+                    id={STRINGS.TAB_NAVIGATION}
+                    onChange={this.handleTabChange}
+                    selectedTabId={this.state.selectedTabId}
+                >
+                    <Tab id={ConfigurationTab.PLAYERS} title={STRINGS.PLAYER_ORDER} />
+                    <Tab id={ConfigurationTab.ROLES} title={STRINGS.ROLES} />
                 </Tabs>
+                {this.renderContent()}
+                {this.maybeRenderStartGameError()}
                 <Button
+                    intent={Intent.SUCCESS}
                     disabled={!this.canStartGame()}
                     onClick={this.startGame}
+                    text={STRINGS.START_GAME}
                 />
             </div>
         );
+    }
+
+    private renderContent() {
+        const { selectedTabId } = this.state;
+        switch (selectedTabId) {
+            case ConfigurationTab.PLAYERS:
+                return <div>{this.renderPlayers()}</div>;
+            case ConfigurationTab.ROLES:
+                return this.renderRoleSelect();
+            default:
+                return assertNever(selectedTabId)
+        }
     }
 
     private renderRoleSelect() {
@@ -140,7 +160,18 @@ export class GameConfiguration extends React.PureComponent<IGameConfigurationPro
         return players.length === roles.length;
     }
 
-    private startGame() {
+    private maybeRenderStartGameError() {
+        if (this.canStartGame()) {
+            return undefined;
+        }
+        const { players } = this.props.game;
+        const { roles } = this.state;
+        const { STRINGS } = GameConfiguration;
+        const message = players.length > roles.length ? STRINGS.NOT_ENOUGH_ROLES : STRINGS.TOO_MANY_ROLES;
+        return (<Callout intent={Intent.DANGER}>{message}</Callout>)
+    }
+
+    private startGame = () => {
         const {
             game: {
                 id: gameId,
