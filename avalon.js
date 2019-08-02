e42955c5c7a2a98b0d58848521a74e36d92c6d5e
Harsh Pandey <hpandey793@gmail.com>
2019-08-01 23:43:39 -0400

    use promises instead of callbacks

--- avalon.js
+++ avalon.js
@@ -1,42 +1,33 @@
 const utils = require('./utils')
 
 exports.api = (model, socket) => ({
-  createGame: (playerName, fn) => {
+  createGame: async (playerName) => {
     const gameId = utils.randomId()
     const playerId = utils.randomId()
     socket.join(playerId)
-    model.createGame(gameId, playerId, playerName, fn)
+    return model.createGame(gameId, playerId, playerName)
   },
-  joinGame: (gameId, playerName, fn) => {
+  joinGame: async (gameId, playerName) => {
     const playerId = utils.randomId()
     socket.join(playerId)
-    model.joinGame(gameId, playerId, playerName, fn)
+    return model.joinGame(gameId, playerId, playerName)
   },
-  rejoinGame: (playerId, fn) => {
-    socket.join(playerId)
-    model.fetchState(playerId, (state) => {
-      if (state) {
-        socket.emit('game', state)
-      }
-      fn({error: state ? null : 'Game not found'})
-    })
-  },
-  leaveGame: (playerId) => {
+  leaveGame: async (playerId) => {
     socket.leave(playerId)
   },
-  startGame: (gameId, playerId, playerName, roleList, playerOrder, fn) => {
-    model.startGame(gameId, playerId, playerName, roleList, playerOrder, fn)
+  startGame: async (gameId, playerId, playerName, roleList, playerOrder) => {
+    return model.startGame(gameId, playerId, playerName, roleList, playerOrder)
   },
-  proposeQuest: (questId, playerId, playerName, proposedMembers, fn) => {
-    model.proposeQuest(questId, playerId, playerName, proposedMembers, fn)
+  proposeQuest: async (questId, playerId, playerName, proposedMembers) => {
+    return model.proposeQuest(questId, playerId, playerName, proposedMembers)
   },
-  voteForProposal: (questId, playerId, playerName, vote, fn) => {
-    model.voteForProposal(questId, playerId, playerName, vote, fn)
+  voteForProposal: async (questId, playerId, playerName, vote) => {
+    return model.voteForProposal(questId, playerId, playerName, vote)
   },
-  voteInQuest: (questId, playerId, playerName, vote, fn) => {
-    model.voteInQuest(questId, playerId, playerName, vote, fn)
+  voteInQuest: async (questId, playerId, playerName, vote) => {
+    return model.voteInQuest(questId, playerId, playerName, vote)
   },
-  assassinate: (gameId, playerId, playerName, target, fn) => {
-    model.assassinate(gameId, playerId, playerName, target, fn)
+  assassinate: async (gameId, playerId, playerName, target) => {
+    return model.assassinate(gameId, playerId, playerName, target)
   }
 })
--- server.js
+++ server.js
@@ -6,17 +6,26 @@ const store = require('./store')
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/db'
 const port = process.env.PORT || 3000
 
-mongodb.MongoClient.connect(mongoUri, {useNewUrlParser: true}, (err, client) => {
-  if (err) {
-    console.error(err)
-  } else {
+mongodb.MongoClient.connect(mongoUri, {useNewUrlParser: true}).then(
+  client => {
     const io = server(port)
 
     const model = store.GameModel(client.db().collection('games'))
     model.onUpdate((playerId, state) => io.to(playerId).emit('game', state))
 
-    io.on('connection', (socket) =>
-      Object.entries(avalon.api(model, socket))
-        .forEach(([name, method]) => socket.on(name, method)))
-  }
-})
+    io.on('connection', (socket) => {
+      Object.entries(avalon.api(model, socket)).forEach(([name, method]) => {
+        socket.on(name, (...args) => {
+          const fn = args.slice(-1)[0]
+          method(...args.slice(0, -1))
+            .then(result => fn({error: null}), error => fn({error}))
+          })
+      })
+      socket.on('rejoinGame', playerId => {
+        socket.join(playerId)
+        model.fetchState(playerId).then(state => socket.emit('game', state))
+      })
+    })
+  },
+  err => console.error(err)
+)
